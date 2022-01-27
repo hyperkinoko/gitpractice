5009df85fb4add71edb8bdba79f7ff2c30782477
Mikoto-TAKASE <t.micoto@gmail.com>
2022-01-27 21:49:02 +0900

    delete np.

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 # print('achachachachachacha')
 
 x = np.array([1,5,10])
-y=np.np.array([2,10,20])
+y=np.array([2,10,20])
 
 fig=plt.figure()
 plt.plot(x,y,color='red')
